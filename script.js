49aaa6c2f97c20cf4f18331d7aa2903ac348de9f
Babek Asadli <babekasadli@protonmail.com>
2022-04-11 05:50:38 -0700

    second commit

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ class TodoService{
     deleteTodo(id){
         this._todos = this._todos.filter(t => t.id !== id);
     }
+
+    _generateID(){
+        return this._todos?.length ? this._todos[this._todos.length - 1].id + 1 : 1;
+    }
 }
 
 const service = new TodoService();
+console.log(service.getTodos());
+service.addTodo('Just do it, NIKE!');
+console.log(service.getTodos());
+service.addTodo('let him cry');
+console.log(service.getTodos());
+service.deleteTodo(service.getTodos().fins(t => t.title === 'let him cry').id);
+console.log('id: ',id);
+service.deleteTodo();
+console.log(service.getTodos());
